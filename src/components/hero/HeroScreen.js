42696ae90c1fb5e51ac3483a997f653de45032d8
RogerCastillo877 <roger.castillo877@gmail.com>
2022-05-23 14:26:22 -0500

    useMemo to prevent innecesary consults to database

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,22 +1,59 @@
-import { Navigate, useParams } from "react-router-dom"
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
   
   const { heroId } = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroById( heroId );
+  const hero = useMemo( () => getHeroById( heroId ), [ heroId] );
 
   if( !hero ) {
     return <Navigate to='/' />
   };
 
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero;
+
+  const imagePath = `/assets/${ id }.jpg`; 
+
+  const handleReturn = () => {
+    navigate(-1)
+  };
+
   return (
-    <div>
-      <h1>HeroScreen</h1>
-      <p>
-        { hero.superhero }
-      </p>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={ imagePath }
+          alt={ superhero }
+          className='img-thumbnail'
+        />
+      </div>
+      <div className="col-8">
+        <h3>{ superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego:</b> { alter_ego }</li>
+          <li className="list-group-item"><b>Publisher:</b> { publisher }</li>
+          <li className="list-group-item"><b>first_appearance:</b> { first_appearance }</li>
+        </ul>
+        <h5 className="mt-5">Characters</h5>
+        <p>{ characters }</p>
+
+        <button
+          className="btn btn-outline-info"
+          onClick={ handleReturn }
+        >
+          Regresar
+        </button>
+      </div>
     </div>
   )
 }
